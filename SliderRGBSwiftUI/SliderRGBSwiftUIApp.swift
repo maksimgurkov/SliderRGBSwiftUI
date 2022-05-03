e2fa852319b88d0d914420746cbfc5b1503bafb3
Maksim Gurkov <maksim.gurkov@gmail.com>
2022-05-03 20:12:00 +0300

    Initial Commit

---
+++ SliderRGBSwiftUI/SliderRGBSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderRGBSwiftUIApp.swift
+//  SliderRGBSwiftUI
+//
+//  Created by Максим Гурков on 03.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderRGBSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
